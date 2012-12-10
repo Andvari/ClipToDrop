ef3f0057225e64f95b08ead4a00d656cef4247b8
Nemo <dedmazai74@mail.ru>
2012-12-10 15:47:13 +0400

    Links saves to Links.txt

--- src/ClipToDrop.py
+++ src/ClipToDrop.py
@@ -12,14 +12,21 @@ import pynotify
 import os
 from datetime import datetime
 
+text = gtk.clipboard_get(gtk.gdk.SELECTION_CLIPBOARD).wait_for_text().encode('utf-8')
+
+filename = "Clipboard.txt"
+
+if (text.find("http:") == 0):
+    filename = "Links.txt"
+
 try:
-    log = open(os.environ['HOME'] + "/Dropbox/Clipboard.txt", "a+")
+    log = open(os.environ['HOME'] + "/Dropbox/" + filename , "a+")
     log.write(str(datetime.now()) + ":\n")
     log.write(gtk.clipboard_get(gtk.gdk.SELECTION_CLIPBOARD).wait_for_text().encode('utf-8'))
     log.write("\n---------------------------------------------------------------------------------------------------------\n\n")
     log.close()
     pynotify.init("Null")
-    n = pynotify.Notification ("Clipboard saved", "", "Null")
+    n = pynotify.Notification ("Clipboard saved", filename, "Null")
     n.show()
 except:
     pynotify.init("Null")
